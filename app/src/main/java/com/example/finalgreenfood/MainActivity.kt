575d883d32e364a3983649eccbd8511d2a652ade
amyeng19 <amyengineer19@gmail.com>
2020-11-27 22:03:59 -0500

    Initial commit!

---
+++ app/src/main/java/com/example/finalgreenfood/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.finalgreenfood
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener {
+            val intent = Intent(this, MainActivity2grow::class.java)
+            startActivity(intent)
+        }
+
+        btn2.setOnClickListener {
+            val intent = Intent(this, MainActivity3buy::class.java)
+            startActivity(intent)
+        }
+    }
+}
